daa693358791a6bac5b776458c52bfcdb08735a8
Jeffrey Nguyen <81765807+98jnguyen@users.noreply.github.com>
2021-06-11 15:05:39 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import CalorieItem from './components/CalorieItem';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <CalorieItem></CalorieItem>
+    </div>
+  );
+}
+
+export default App;
