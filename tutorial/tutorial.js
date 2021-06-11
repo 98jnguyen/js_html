a074133cbd973be5c102a649b6990cc2cb4827ca
Jeffrey Nguyen <81765807+98jnguyen@users.noreply.github.com>
2021-06-11 15:07:34 -0700

    Create tutorial.js

---
+++ tutorial/tutorial.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+
+
+
+// class Human {
+//     constructor(){
+//         this.gender = 'male';
+//     }
+
+//     printGender(){
+//         console.log(this.gender);
+//     }
+// }
+
+// class Me extends Human {
+//     constructor(){
+//         super();
+//         this.name = 'Jeffrey';
+//     }
+
+//     printName(){
+//         console.log(this.name);
+//     }
+// }
+
+
+// const gen = new Me();
+
+// gen.printGender();
+// gen.printName();
+
+// const numbers = [1,2,3];
+// const newnum = [...numbers, 4, 5,6];
+// console.log(newnum);
+
+// const person = {
+//     name:'Jeffrey',
+//     age:24
+// };
+
+// const newPerson = {
+//     ...person, occupation:"Engineer"
+// };
+
+
+// console.log(newPerson);
+
+
+// class newName {
+//     constructor(){
+//         this.Name = 'Papi Munonon';
+//     }
+
+//     printName(){
+//         console.log(this.Name);
+//     }
+// }
+
+// const person = new newName();
+// person.printName();
+
+
+
+num = {
+    name:'Jeffrey',
+    age:24,
+    occupation:'Engineer'
+}
+
+
+
+console.log(num.age);
